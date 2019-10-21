1f9879d5e78ecf5e79e93f03f5800f932deb091e
Björn <out.bjorn@gmail.com>
2019-10-21 14:23:38 +0200

    added check for initializer

--- cards/__init__.py
+++ cards/__init__.py
@@ -3,9 +3,14 @@ import uva.check50.py
 import check50.internal
 import sys
 import os
+import inspect
+import random
 
 check50.internal.register.before_every(lambda : sys.path.append(os.getcwd()))
 check50.internal.register.after_every(lambda : sys.path.pop())
+
+suits = ['Hearts','Diamonds','Clubs','Spades']
+values = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
 
 def class_exists(module, cls):
     if not hasattr(module, cls):
@@ -16,6 +21,14 @@ def properties_present(cls, attributes):
     for attribute in attributes:
         if not hasattr(cls, attribute):
             raise check50.Failure(f"expected class '{cls.__name__}' to have property '{attribute}'")
+
+
+def initializer_arguments(cls, required_args):
+    args = inspect.getfullargspec(cls).args
+
+    for arg in required_args:
+        if not arg in args:
+            raise check50.Failure(f"expected initializer for class '{cls.__name__}' to accept argument '{arg}'")
 
 
 @check50.check()
@@ -32,10 +45,30 @@ def compiles():
 
 
 @check50.check(compiles)
-def card_class_basic(stdout):
-    """class Card exists and has basic attributes."""
+def card_class_basic():
+    """class 'Card' exists and has basic attributes."""
     module = uva.check50.py.run("cardgame.py").module
     class_exists(module, "Card")
 
+    # check if the class has the required properties
     properties = ["suit", "value"]
     properties_present(module.Card, properties)
+
+
+@check50.check(card_class_basic)
+def card_initializer():
+    """class 'Card' can be initialized correctly."""
+    module = uva.check50.py.run("cardgame.py").module
+
+    # check if __init__ accepts the correct args
+    required_args = ["self", "suit", "value"]
+    initializer_arguments(module.Card, required_args)
+
+    # initialize a random card
+    random_suit = random.choice(suits)
+    random_value = random.choice(values)
+    card = module.Card(suit=random_suit, value=random_value)
+
+    # check if the initializer worked
+    if card.suit != random_suit:
+        raise check50.Failure(f"class 'Card' was initialized with unexpected suit '{card.suit}'. expected '{random_suit}'")
