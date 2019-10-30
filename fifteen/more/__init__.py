aea3b503b731997f49eb0d60c0483cf2c47db69c
Björn <out.bjorn@gmail.com>
2019-10-30 12:19:50 +0100

    god mode doesn't need to print ftw

--- fifteen/more/__init__.py
+++ fifteen/more/__init__.py
@@ -6,11 +6,11 @@ from less import *
 @check50.check(solve4)
 def god_solve4():
     """god mode solves a 4x4 board from starting position."""
-    board = " *1 *2 *3 *4 *\\n\\n *5 *6 *7 *8 *\\n\\n *9 *10 *11 *12 *\\n\\n *13 *14 *15 *\_ *\\n\\n *ftw\! *\\n"
+    board = " *1 *2 *3 *4 *\\n\\n *5 *6 *7 *8 *\\n\\n *9 *10 *11 *12 *\\n\\n *13 *14 *15 *\_ *"
 
     # at least one of the below must be reached at some point
-    intermediate_1 = " *1 *2 *3 *4 *\\n\\n *5 *6 *7 *8 *\\n\\n *9 *10 *11 *12 *\\n\\n *13 *14 *\_ *15 *\\n\\n *ftw\! *\\n"
-    intermediate_2 = " *1 *2 *3 *4 *\\n\\n *5 *6 *7 *8 *\\n\\n *9 *10 *11 *\_ *\\n\\n *13 *14 *15 *12 *\\n\\n *ftw\! *\\n"
+    intermediate_1 = " *1 *2 *3 *4 *\\n\\n *5 *6 *7 *8 *\\n\\n *9 *10 *11 *12 *\\n\\n *13 *14 *\_ *15 *"
+    intermediate_2 = " *1 *2 *3 *4 *\\n\\n *5 *6 *7 *8 *\\n\\n *9 *10 *11 *\_ *\\n\\n *13 *14 *15 *12 *"
 
     # run god mode and get output
     check = check50.run("./fifteen 4")
@@ -37,7 +37,7 @@ def god_solve3():
              "3","8","6","5","2","3","6","1",
              "7","6","3","2","5","3","6","4"]
 
-    board = " *1 *2 *3 *\\n\\n *4 *5 *6 *\\n\\n *7 *8 *\_ *\\n\\n *ftw\! *\\n"
+    board = " *1 *2 *3 *\\n\\n *4 *5 *6 *\\n\\n *7 *8 *\_ *"
 
     check = check50.run("./fifteen 4")
 
